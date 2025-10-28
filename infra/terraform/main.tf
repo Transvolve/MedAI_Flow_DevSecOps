f36eb8e8531f45ad3057376273adf720319f72a7
Transvolve <transvolveconsultancy@gmail.com>
2025-10-28 23:35:20 +0000

    Add MedAI_Flow_DevSecOps project scaffolding with backend, infrastructure, and documentation structure

---
+++ infra/terraform/main.tf
@@ -0,0 +1,68 @@
+# ------------------------------------------------------------
+# Terraform IaC for Azure AKS + ACR (MedAI_Flow_DevSecOps)
+# ------------------------------------------------------------
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# ---------------- Resource Group ----------------
+resource "azurerm_resource_group" "medai_rg" {
+  name     = "rg-medai-flow"
+  location = "westeurope"
+}
+
+# ---------------- Container Registry ----------------
+resource "azurerm_container_registry" "medai_acr" {
+  name                = "medaiflowacr"
+  resource_group_name = azurerm_resource_group.medai_rg.name
+  location            = azurerm_resource_group.medai_rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# ---------------- AKS Cluster ----------------
+resource "azurerm_kubernetes_cluster" "medai_aks" {
+  name                = "medai-aks-cluster"
+  location            = azurerm_resource_group.medai_rg.location
+  resource_group_name = azurerm_resource_group.medai_rg.name
+  dns_prefix          = "medaiflow"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+  tags = {
+    Environment = "DevSecOps"
+    Project     = "MedAI_Flow"
+  }
+}
+
+# ---------------- Outputs ----------------
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.medai_aks.kube_config_raw
+  sensitive = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.medai_acr.login_server
+}
